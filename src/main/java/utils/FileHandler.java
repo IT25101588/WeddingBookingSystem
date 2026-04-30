10810160bc9b9a47477d938b7f6e2f75fcadf696
Rahman M Z A <it25102270@my.sliit.lk>
2026-05-01 00:02:42 +0530

    filehander changes

--- src/main/java/utils/FileHandler.java
+++ src/main/java/utils/FileHandler.java
@@ -10,3 +10,14 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+public class FileHandler {
+
+    // ==========================================
+    // SYSTEM FILE PATHS
+    // ==========================================
+    private static final String BASE_PATH = "C:\\WeddingData\\";
+    private static final String USER_FILE = BASE_PATH + "users.txt";
+    private static final String VENDOR_FILE = BASE_PATH + "vendors.txt";
+    private static final String VENUE_FILE = BASE_PATH + "venues.txt";
+    private static final String BOOKING_FILE = BASE_PATH + "bookings.txt";
+    private static final String REVIEW_FILE = BASE_PATH + "reviews.txt";
