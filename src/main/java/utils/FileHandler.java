177ed3d183793dc9060c20d619fc9cdee9f2d420
Rahman M Z A <it25102270@my.sliit.lk>
2026-05-01 00:04:26 +0530

    filehander changes

--- src/main/java/utils/FileHandler.java
+++ src/main/java/utils/FileHandler.java
@@ -21,3 +21,4 @@ public class FileHandler {
     private static final String VENUE_FILE = BASE_PATH + "venues.txt";
     private static final String BOOKING_FILE = BASE_PATH + "bookings.txt";
     private static final String REVIEW_FILE = BASE_PATH + "reviews.txt";
+
